940ac1be1659b7812d3958d9b46dd0dfdd1dc2ed
wmboon <wietseboon@gmail.com>
2024-05-03 13:44:36 +0200

    finished 3D Dirichlet bcs

--- TPSA.py
+++ TPSA.py
@@ -1,23 +1,54 @@
 import numpy as np
 import scipy.sparse as sps
-import pygeon as pg
 
 
 class TPSA:
-    def __init__(self, sd, mu, l2) -> None:
+    def __init__(self) -> None:
+        pass
 
-        find_cf = sps.find(sd.cell_faces)
+    def dim_r(self, sd):
+        return sd.dim * (sd.dim - 1) // 2
+
+    def assemble(self, sd, mu, l2, labda):
+
+        sigma = self.primary_to_dual_map(sd, mu, l2)
+        div = self.div_map(sd)
+
+        A = div @ sigma
+
+        M = self.mass(sd, mu, labda)
+
+        return A - M
+
+    def primary_to_dual_map(self, sd, mu, l2):
+        cf = sps.csc_array(sd.cell_faces)
+        find_cf = sps.find(cf)
 
         self.delta_ki = self.assemble_delta_ki(sd, find_cf)
         # delta_k = np.bincount(faces, weights=delta_ki)
 
         mu_bar = self.harmonic_avg(find_cf, mu)
+        A_uu = -2 * mu_bar[:, None] * cf
+        A_uu = sps.block_diag([A_uu] * sd.dim)
+
         l2_bar = self.harmonic_avg(find_cf, l2)
+        A_rr = -l2_bar[:, None] * cf
+        A_rr = sps.block_diag([A_rr] * self.dim_r(sd))
+
         dk_mu = self.assemble_dk_mu(find_cf, mu, 2)
+        A_pp = -dk_mu[:, None] * cf
 
         chi, chi_tilde = self.assemble_chis(find_cf, mu)
 
-        pass
+        A_ru = self.assemble_S_star(sd, chi, True)
+        A_ur = self.assemble_S_star(sd, chi_tilde, False)
+
+        A_pr = self.assemble_n_chi(sd, chi, True)
+        A_rp = self.assemble_n_chi(sd, chi_tilde, False)
+
+        return sps.block_array(
+            [[A_uu, A_ur, A_rp], [A_ru, A_rr, None], [A_pr, None, A_pp]]
+        )
 
     def assemble_delta_ki(self, sd, find_cf):
         faces, cells, orient = find_cf
@@ -54,10 +85,56 @@ class TPSA:
     def assemble_chis(self, find_cf, mu):
         faces, cells, _ = find_cf
 
-        Chi = sps.csc_array((mu, (faces, cells)))
+        Chi = sps.csc_array((mu[cells], (faces, cells)))
         Chi /= Chi.sum(axis=1)[:, None]
 
         Chi_tilde = Chi.copy()
         Chi_tilde.data = 1 - Chi_tilde.data
 
         return Chi, Chi_tilde
+
+    def assemble_S_star(self, sd, chi, u_to_r=True):
+        nx, ny, nz = [n_i[:, None] for n_i in sd.face_normals]
+
+        if sd.dim == 3:
+            return -sps.block_array(
+                [
+                    [None, -nz * chi, ny * chi],
+                    [nz * chi, None, -nx * chi],
+                    [-ny * chi, nx * chi, None],
+                ],
+                format="csc",
+            )
+        elif sd.dim == 2:
+            if u_to_r:
+                return -sps.hstack(
+                    [-ny * chi, nx * chi],
+                    format="csc",
+                )
+            else:
+                return -sps.vstack(
+                    [ny * chi, -nx * chi],
+                    format="csc",
+                )
+        else:
+            raise NotImplementedError("Dimension must be 2 or 3.")
+
+    def assemble_n_chi(self, sd, chi, u_to_p=True):
+        normals = [n_i[:, None] for n_i in sd.face_normals]
+        normal_times_chi = [n_i * chi for n_i in normals[: sd.dim]]
+
+        if u_to_p:
+            return sps.hstack(normal_times_chi, format="csc")
+        else:
+            return sps.vstack(normal_times_chi, format="csc")
+
+    def div_map(self, sd):
+        dim = sd.dim + self.dim_r(sd) + 1
+        return sps.block_diag([sd.cell_faces.T] * dim, format="csc")
+
+    def mass(self, sd, mu, labda):
+        M_u = sps.csc_array((sd.dim * sd.num_cells, sd.dim * sd.num_cells))
+        M_r = sps.diags_array(np.tile(sd.cell_volumes / mu, self.dim_r(sd)))
+        M_p = sps.diags_array(sd.cell_volumes / labda)
+
+        return sps.block_diag([M_u, M_r, M_p])
--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import scipy.sparse as sps
+import os
 import numpy as np
+import porepy as pp
 import pygeon as pg
 
 from TPSA import TPSA
 
-mdg = pg.unit_grid(2, 0.5)
+mdg = pg.unit_grid(3, 0.1)
 mdg.compute_geometry()
 sd = mdg.subdomains()[0]
 
 mu = np.ones(sd.num_cells)
 l_squared = np.ones(sd.num_cells)
+labda = np.ones(sd.num_cells)
 
-disc = TPSA(sd, mu, l_squared)
+disc = TPSA()
+A = disc.assemble(sd, mu, l_squared, labda)
 
+rhs = np.zeros(A.shape[0])
+rhs[(sd.dim - 1) * sd.num_cells : sd.dim * sd.num_cells] = sd.cell_volumes
 
-pass
+x = sps.linalg.spsolve(A, rhs)
+
+indices = sd.num_cells * np.cumsum([sd.dim, disc.dim_r(sd)])
+u, r, p = np.split(x, indices)
+
+if sd.dim == 2:
+    # we need to add the z component for the exporting
+    u = np.hstack((u, np.zeros(sd.num_cells)))
+else:
+    r = r.reshape((3, -1))
+
+u = u.reshape((3, -1))
+
+folder = os.path.dirname(os.path.abspath(__file__))
+save = pp.Exporter(sd, "sol_TPSA", folder_name=folder)
+save.write_vtu([("u", u), ("r", r), ("p", p)])
